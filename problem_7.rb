f92849889bb306216194c8f69f6cb25f32461fb1
Colin Armstrong <colinwarmstrong@gmail.com>
2018-12-02 19:05:41 -0700

    Complete problem 7

---
+++ problem_7.rb
@@ -0,0 +1,20 @@
+# By listing the first six prime numbers:
+# 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#
+# What is the 10001st prime number?
+
+require 'prime'
+
+solution = 0
+i = 1
+
+Prime.each do |prime|
+  if i == 10001
+    solution = prime
+    break
+  else
+    i += 1
+  end
+end
+
+puts solution
