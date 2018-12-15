82c159742d8f152ad7d4b9e3c39df8e3f6553e31
Colin Armstrong <colinwarmstrong@gmail.com>
2018-12-15 16:34:09 -0700

    Refactor problem 48

--- problem_48.rb
+++ problem_48.rb
@@ -3,8 +3,6 @@
 # Find the last ten digits of the series,
 # 1^1 + 2^2 + 3^3 + ... + 10001000.
 
-sum = (1..1000).sum do |int|
-  int**int
-end
+sum = (1..1000).sum { |int| int**int }
 
 puts sum.to_s[-10..-1]
